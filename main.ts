26e5391ab4f9b91c7bad2a7df5c22768202c1e57
stempunks <58650300+stempunks@users.noreply.github.com>
2020-02-25 08:18:36 +1000

    Update main.ts

--- main.ts
+++ main.ts
@@ -20,7 +20,7 @@ enum Newline {
 /**
  * WiFi:bit commands.
  */
-//% color=#3452C3 weight=90 icon="\uf1eb" block="WiFi:bit"
+//% color=#000000 weight=90 icon="\uf1eb" block="WiFi:bit"
 namespace WiFiBit {
 
     function writeToSerial(data: string, waitTime: number): void {
